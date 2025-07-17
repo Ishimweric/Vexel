c129d82e94d10ffbd72b16114d25f9dcdfa9ef10
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-17 12:09:00 +0200

    Fix: Fixed the header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ function Header(props){
   return (
     <header className="sticky top-0 w-full bg-white border-b h-16 shadow-sm">
       <article className="flex items-center justify-between px-4">
-        <img src="images/vexel-logo.png" className="w-16"/>
+        <div className="flex gap-2 items-center">
+          <img src="images/vexel-logo.png" className="w-16"/>
+          <p className="text-2xl text-pink-700 font-bold">Vexel</p>
+        </div>
         {!props.showStickyCta && <StartQuizBtn/>}
       </article>
     </header>
